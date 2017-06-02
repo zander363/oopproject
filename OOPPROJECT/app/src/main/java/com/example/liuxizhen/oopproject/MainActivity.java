d219749210cd1b515ab0e875f4de22710b7a9083
b04505009 <b04505009@ntu.edu.tw>
2017-06-02 22:33:41 +0800

    change illegal widgets' id. haven't constrain all the widgets yet

--- OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/MainActivity.java
+++ OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/MainActivity.java
@@ -4,31 +4,58 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
+import android.widget.ImageButton;
 
 
 public class MainActivity extends AppCompatActivity {
+    private ImageButton booking;
+    private ImageButton refund;
+    private ImageButton conditional_booking;
+    private ImageButton search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //取得此Button的實體
-        Button button = (Button) findViewById(R.id.button);
+        booking = (ImageButton) findViewById(R.id.booking);
+        refund = (ImageButton) findViewById(R.id.refund);
+        conditional_booking = (ImageButton) findViewById(R.id.conditional_booking);
+        search = (ImageButton) findViewById(R.id.search);
 
-        //實做OnClickListener界面
-        button.setOnClickListener(new View.OnClickListener() {
+        booking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //初始化Intent物件
                 Intent intent = new Intent();
-                //從MainActivity 到booking1
                 intent.setClass(MainActivity.this, booking1.class);
-                //開啟Activity
                 startActivity(intent);
             }
         });
+        refund.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, refund1.class);
+                startActivity(intent);
+            }
+        });
+        conditional_booking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, conditional_booking1.class);
+                startActivity(intent);
+            }
+        });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, search.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
 
--- OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/search_movie_ID2.java
+++ OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/search_movie_ID2.java
@@ -3,11 +3,11 @@ package com.example.liuxizhen.oopproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class search_movie_ID2 extends AppCompatActivity {
+public class search_movie_id2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_movie_ID2);
+        setContentView(R.layout.search_movie_id2);
     }
 }
