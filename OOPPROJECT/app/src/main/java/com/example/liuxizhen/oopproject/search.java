789adab258bf528056b6202bab10e92510727ef9
b04505009 <b04505009@ntu.edu.tw>
2017-06-05 21:50:43 +0800

    timepicker of search_time1

--- OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/search.java
+++ OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/search.java
@@ -1,13 +1,27 @@
 package com.example.liuxizhen.oopproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class search extends AppCompatActivity {
+    private ImageButton time ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
+        time = (ImageButton) findViewById(R.id.time);
+
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(search.this, search_time1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/search_time1.java
+++ OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/search_time1.java
@@ -2,12 +2,73 @@ package com.example.liuxizhen.oopproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class search_time1 extends AppCompatActivity {
+    private Button start_time;
+    private Button end_time;
+    private TextView timetext2;
+    private TextView timetext;
+    private TimePickerDialog timePickerDialog2;
+    private TimePickerDialog timePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_time1);
+        doFindView();
+        GregorianCalendar calendar = new GregorianCalendar();
+
+
+        // 實作TimePickerDialog的onTimeSet方法，設定時間後將所設定的時間show在textTime上
+        timePickerDialog = new TimePickerDialog(this, new OnTimeSetListener() {
+            //將時間轉為12小時製顯示出來
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                timetext.setText((hourOfDay > 12 ? hourOfDay - 12 : hourOfDay)
+                        + ":" + minute + " " + (hourOfDay > 12 ? "PM" : "AM"));
+            }
+        }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(calendar.MINUTE),
+                false);
+        // 實作TimePickerDialog的onTimeSet方法，設定時間後將所設定的時間show在textTime上
+        timePickerDialog2 = new TimePickerDialog(this, new OnTimeSetListener() {
+            //將時間轉為12小時製顯示出來
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                timetext2.setText((hourOfDay > 12 ? hourOfDay - 12 : hourOfDay)
+                        + ":" + minute + " " + (hourOfDay > 12 ? "PM" : "AM"));
+            }
+        }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(calendar.MINUTE),
+                false);
     }
+
+
+    // 連接layout上的物件
+    public void doFindView() {
+        start_time = (Button) findViewById(R.id.timestart);
+        end_time = (Button) findViewById(R.id.timeend);
+        timetext =(TextView) findViewById(R.id.time);
+        timetext2 =(TextView) findViewById(R.id.time2);
+    }
+
+
+
+    // setTime Button onClick 顯示時間設定視窗
+    public void setTime(View v) {
+        timePickerDialog.show();
+    }
+    // setTime Button onClick 顯示時間設定視窗
+    public void setTime2(View v) {
+        timePickerDialog2.show();
+    }
+
 }
+
+
