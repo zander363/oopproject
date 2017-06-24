2c2ab60a553d2cc81398d74df79f7df40f5e7956
b04505009 <b04505009@ntu.edu.tw>
2017-06-25 03:01:51 +0800

    refund

--- OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/refund1.java
+++ OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/refund1.java
@@ -5,20 +5,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class refund1 extends AppCompatActivity {
     private Button confirmation;
+    private EditText input_ID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.refund1);
         confirmation = (Button) findViewById(R.id.confirmation);
+        input_ID = (EditText) findViewById(R.id.input_ID) ;
 
         confirmation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setClass(refund1.this, refund2.class);
+                intent.putExtra("input_ID",input_ID.getText().toString());
                 startActivity(intent);
             }
         });
--- OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/refund2.java
+++ OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/refund2.java
@@ -5,21 +5,43 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import basicMethod.*;
 
 public class refund2 extends AppCompatActivity {
     private Button confirmation;
+    private TextView refund_result;
+    private String input_ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.refund2);
         confirmation = (Button) findViewById(R.id.confirmation);
+        refund_result = (TextView) findViewById(R.id.refund_result);
+
+        Bundle bundle = this.getIntent().getExtras();
+        input_ID = bundle.getString("input_ID");
+
+
+        switch(BookAndRefund.refundReportCode(input_ID)){
+            case 0:
+                refund_result.setText("退票成功");
+                break;
+            case 1:
+                refund_result.setText("電影將於20分鐘內上映，恕無法退票");
+                break;
+            case 2:
+                refund_result.setText("查無此票卡");
+
+        }
 
         confirmation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setClass(refund2.this, MainActivity.class);
+
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             }
