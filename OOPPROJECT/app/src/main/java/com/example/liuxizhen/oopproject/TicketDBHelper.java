519e9bb796361e6b051fd7aa098261f8f33b2d7a
zander363 <b04505021@ntu.edu.tw>
2017-06-26 03:29:22 +0800

    debug for DBHelper

--- OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/TicketDBHelper.java
+++ OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/TicketDBHelper.java
@@ -18,24 +18,22 @@ public class TicketDBHelper extends SQLiteOpenHelper {
 
     public static SQLiteDatabase getDatabase(Context context) {
         if (database == null || !database.isOpen()) {
-            database = new TicketDBHelper(context, DATABASE_NAME, 
+            database = new TicketDBHelper(context, DATABASE_NAME,
                     null, VERSION).getWritableDatabase();
+        }
+        return database;
     }
+        @Override
+        public void onCreate(SQLiteDatabase db) {
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
+        }
 
-        db.execSQL(com.example.liuxizhen.oopproject.OOPTicket.CREATE_TABLE);
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+            onCreate(db);
+        }
 
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion) {
-	    db.execSQL("DROP TABLE IF EXISTD"+"oop.user");
-	    onCreate(db);
-
-    }
 
 }
 
--- OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/UserDBHelper.java
+++ OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/UserDBHelper.java
@@ -16,25 +16,22 @@ public class UserDBHelper extends SQLiteOpenHelper {
 
     public static SQLiteDatabase getDatabase(Context context) {
         if (database == null || !database.isOpen()) {
-            database = new UserDBHelper(context, DATABASE_NAME, 
+            database = new UserDBHelper(context, DATABASE_NAME,
                     null, VERSION).getWritableDatabase();
+        }
+        return database;
     }
-
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL(com.example.liuxizhen.oopproject.OOPUser.CREATE_TABLE);
-
-
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db,
-                          int oldVersion, int newVersion) {
-	    db.execSQL("DROP TABLE IF EXISTD"+"oop.user");
-	    onCreate(db);
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        onCreate(db);
     }
+
 
 }
 
