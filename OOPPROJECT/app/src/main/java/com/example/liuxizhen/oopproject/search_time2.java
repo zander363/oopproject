3946900365a3cdb23f76cd2407bf1f05e8417249
zander363 <b04505021@ntu.edu.tw>
2017-06-26 13:09:45 +0800

    search time

--- OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/search_time2.java
+++ OOPPROJECT/app/src/main/java/com/example/liuxizhen/oopproject/search_time2.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import basicClass.Movie;
+import basicMethod.Base;
 
 public class search_time2 extends AppCompatActivity {
     private Button confirmation;
@@ -14,6 +21,30 @@ public class search_time2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_time2);
         confirmation = (Button) findViewById(R.id.confirmation);
+        TextView search_time_result=(TextView)findViewById(R.id.search_time_result);
+        ArrayList<Movie> movies=new ArrayList<>();
+        int i;
+        for(i=0; i< Base.moviesList.size(); i++){
+            if(start_time.isearlierto(Base.moviesList.get(i).time){
+                movies.add(Base.moviesList.get(i));
+            }
+        }
+        for(i=0; i< Base.moviesList.size(); i++){
+            if(end_time.isearlierto(Base.moviesList.get(i).time){
+                movies.remove(i);
+            }
+        }
+        if(movies.size()>=0) {
+            String text="";
+            for(i=0; i< movies.size(); i++) {
+                text += movies.get(i).moviename+"\n";
+            }
+            search_time_result.setText(text);
+        }
+        else{
+            search_time_result.setText("查無此號");
+        }
+
 
         confirmation.setOnClickListener(new View.OnClickListener() {
             @Override
